2e04ad871c87ef51d25936286a1e5ecaf538275d
Remon Agayby <agayby.remon.com>
2023-12-26 07:30:00 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <h1>My Vue App</h1>
+        <button>Add Task</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App'
+    }
+</script>
+
+<style>
+    #app {
+    text-align: center;
+    color: chocolate;
+    margin-top: 60px;
+    }
+
+    .container {
+        border: 1px solid steelblue;
+        padding: 30px;
+        max-width: 500px;
+        margin: auto;
+    }
+
+    button {
+        padding: 10px 20px;
+        font-size: 15px;
+        background-color: brown;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+</style>
